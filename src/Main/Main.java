c19ffc555e7c93f8b6dad9f76eee51554499d982
TKimYen <148522276+TKimYen@users.noreply.github.com>
2024-05-02 14:39:29 +0700

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,35 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Main;
 
-import cuahangbanpizza.DAO.MyConnect;
-import cuahangbanpizza.GUI.Sign_in;
-//import cuahangbanpizza.GUI.DangNhapGUI;
+import QuanLyPizza.DAO.MyConnect;
+import QuanLyPizza.GUI.DangNhapGUI;
 
 public class Main {
 
-    public Main(){
-//       new MyConnect();
+    public static void main(String[] args) throws ClassNotFoundException {
+        new MyConnect();
 
-//        changLNF("Nimbus");
-        new Sign_in();
-        
-    }
-    public static void main(String[] args) {
-        new Main();
+        changLNF("Nimbus");
+        DangNhapGUI login = new DangNhapGUI();
+        login.showWindow();
     }
 
-    public void changLNF(String nameLNF) {
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if (nameLNF.equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-        }
+    public static void changLNF(String nameLNF) {
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if (nameLNF.equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+//        }
     }
 }
